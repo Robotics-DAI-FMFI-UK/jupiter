e8e3931a150ee16df8d0377bfd8ddf7e679f4cf1
Granko15 <granko15@gmail.com>
2023-06-16 01:59:57 +0200

    add arm_to_xyz.cpp

---
+++ emma/src/emma/src/learning/arm_to_xyz.cpp
@@ -0,0 +1,98 @@
+#include "ros/ros.h"
+#include "std_msgs/Float64.h"
+
+#include <sstream>
+#include <tuple>
+#include <cstdio>
+#include "jupiter_arm_kinematics.h"
+
+using namespace std;
+
+/**
+ simple example of controlling arm by publishing messages to dynamixel
+ * arm controller. First, run
+ *   roslaunch rchomeedu_arm arm.launch
+ * and then
+ *   rosrun emma move_arm
+ * 
+ * the angle is in radians, we send alternating
+ * requests to move waist to positions 60deg, 120deg 
+ * every 3 seconds.
+ */
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "move_arm");
+
+  ros::NodeHandle n;
+  ros::Publisher arm0_pub = n.advertise<std_msgs::Float64>("/waist_controller/command", 100);
+  ros::Publisher arm1_pub = n.advertise<std_msgs::Float64>("/shoulder_controller/command", 100);
+  ros::Publisher arm2_pub = n.advertise<std_msgs::Float64>("/elbow_controller/command", 100);
+  ros::Publisher arm3_pub = n.advertise<std_msgs::Float64>("/wrist_controller/command", 100);
+  ros::Publisher arm4_pub = n.advertise<std_msgs::Float64>("/hand_controller/command", 100);
+
+  ros::Rate loop_rate(10);
+  
+  int q = 1;
+  int cnt = 0;
+
+    double x,y,z,l1,l2,l3,pi;
+
+    printf("x=");
+    scanf("%lf", &x);
+    printf("y=");
+    scanf("%lf", &y);
+    printf("z=");
+    scanf("%lf", &z);
+    printf("l1=");
+    scanf("%lf", &l1);
+    printf("l2=");
+    scanf("%lf", &l2);
+    printf("l3=");
+    scanf("%lf", &l3);
+    printf("pi[deg]=");
+    scanf("%lf", &pi);
+    pi *= M_PI / 180.0;
+
+    tuple<double, double, double, double> angles = findArmAngles(x,y,z,l1,l2,l3,pi);
+
+    printf("---------------\na1=%.3lf (%.1lf deg)\na2=%.3lf (%.1lf deg)\na3=%.3lf (%.1lf deg)\na4=%.3lf (%.1lf deg)\n",
+              get<0>(angles),
+              get<0>(angles) * 180.0 / M_PI,
+              get<1>(angles),
+              get<1>(angles) * 180.0 / M_PI,
+              get<2>(angles),
+              get<2>(angles) * 180.0 / M_PI,
+              get<3>(angles),
+              get<3>(angles) * 180.0 / M_PI);
+
+    std_msgs::Float64 msg;
+
+    msg.data = -get<0>(angles);
+    arm0_pub.publish(msg);
+    msg.data = get<1>(angles);
+    arm1_pub.publish(msg);
+    msg.data = get<2>(angles);
+    arm2_pub.publish(msg);
+    msg.data = get<3>(angles);
+    arm3_pub.publish(msg);
+
+
+  while (ros::ok())
+  {
+    /**
+     * This is a message object. You stuff it with data, and then publish it.
+     */
+     if (cnt++ == 30)
+     {
+		 cnt = 0;
+     }
+    ros::spinOnce();
+
+    loop_rate.sleep();
+  }
+
+
+  return 0;
+}
+
---
+++ emma/src/emma/src/learning/jupiter_arm_kinematics.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+#include <tuple>
+
+using namespace std;
+
+tuple<double, double, double, double> findArmAngles(double x, double y, double z, double l1, double l2, double l3, double pi) {
+    double alpha = atan2(y, x);// Uhol, o ktory sa musi rameno natocit k objektu
+
+    //nove suradnice x, y
+    double d = sqrt(x*x + y*y);
+    y = z;
+    x = d;
+    
+    double m = l3 * sin(pi);
+    double n = l3 * cos(pi);//sqrt(l1 * l1 - m * m);
+
+    //dalej pracujeme v rovine x, y
+
+    double cx, cy;
+    cx = x - m;
+    cy = y + n; 
+    
+    double r = sqrt(cx * cx + cy * cy);
+
+    //vyjadrene z kosinusovej vety (c^2 = a^2 + b^2 - 2ab * cos(C))
+    //2ab * cos(C) = a^2 + b^2 - c^2                 
+    //cos(C) = (a^2 + b^2 - c^2) / 2ab
+    // C = acos((a^2 + b^2 - c^2) / 2ab)
+    double phi = acos((l1 * l1 + r * r - l2 * l2) / (2 * l1 * r));
+
+    double omega = acos(cx / r);  //acos((cx * cx + r * r - cy * cy) / (2 * cx * r));
+    double theta2 = M_PI/2 - phi - omega;
+
+    double epsilon = acos((l1 * l1 + l2 * l2 - r * r) / (2 * l1 * l2));
+    double beta = M_PI - epsilon;
+
+    double delta = M_PI - theta2 - beta - pi;    //psi = 2*M_PI - alpha - epsilon - omega - M_PI/2;
+
+    //double delta = M_PI - psi - pi;
+
+    return make_tuple(alpha, theta2, beta, delta);
+}
+
+
